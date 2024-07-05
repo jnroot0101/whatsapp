741bcb36e495223db729e7df679c1a68dc1ac7d8
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2024-07-05 20:09:14 +0100

    Initial Commit

---
+++ whatsapp/whatsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsappApp.swift
+//  whatsapp
+//
+//  Created by Redouane Jnah on 4/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct whatsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
