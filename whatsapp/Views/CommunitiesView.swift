998aa2c83b8b49acd1a042adc5ce84d7cba7c795
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2024-07-05 22:00:20 +0100

    Implement tabview UI

---
+++ whatsapp/Views/CommunitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  CommunitiesView.swift
+//  whatsapp
+//
+//  Created by Redouane Jnah on 5/7/2024.
+//
+
+import SwiftUI
+
+struct CommunitiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunitiesView()
+}
