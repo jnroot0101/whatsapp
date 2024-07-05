0d5118812a8625cdfb75d171d5311c709bb9d0d2
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2024-07-05 22:05:27 +0100

    Add update view to tabView

---
+++ whatsapp/Views/UpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateView.swift
+//  whatsapp
+//
+//  Created by Redouane Jnah on 5/7/2024.
+//
+
+import SwiftUI
+
+struct UpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateView()
+}
